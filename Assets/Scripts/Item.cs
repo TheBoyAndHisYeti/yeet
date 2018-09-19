9b8623c3dd30ee8f08f045073199e559d8870f36
Furkan Cirik <furkan.cirik@elev.ga.lbs.se>
2018-09-19 14:37:55 +0200

    Init Unity
    yeet

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string displayName;
+    public float Weight;
+    public float Cost;
+
+    public virtual void OnMouseEnter()
+    {
+        Debug.Log(ToString());
+    }
+
+    public override string ToString()
+    {
+        return displayName + " costs " + Cost + " gold " + "per " + Weight + "kg";
+    }
+
+
+}
+
---
+++ Assets/Scripts/Usable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usable : Item
+{
+    private int use;
+
+    public int maxUses;
+    public string usetext;
+
+    public void Start()
+    {
+        use = maxUses;
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log(ToString());
+        use--;
+    }
+
+    public virtual void Use()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return displayName + " has " + use + " out of " + maxUses + "uses";
+    }
+
+
+}
